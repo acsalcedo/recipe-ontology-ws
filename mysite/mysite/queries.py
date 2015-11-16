a563cbe6d0b0354b04dc28b0b89ce67a940a383f
Andrea Salcedo <andreacsalcedo@gmail.com>
2015-11-16 09:59:46 -0430

    Agregué un archivo css.

---
+++ mysite/mysite/queries.py
@@ -0,0 +1,57 @@
+from json2html import *
+import requests
+import json
+
+prefix = """
+PREFIX recipe: <http://www.semanticweb.org/andrea/ontologies/2015/10/recipes#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+"""
+
+def q1():
+
+    query1 = """
+    SELECT ?recipe_name ?predicate ?object
+    WHERE {
+    ?subject recipe:Recipe_Name ?recipe_name.
+    ?subject recipe:Preparation_Time ?object.
+      {?subject recipe:Has recipe:Corn_Flour}
+     UNION
+    {?subject recipe:Has recipe:Flour}
+    } ORDER BY ?object
+    """
+    payload1 = {'query': prefix + query1}
+    r1 = requests.get("http://localhost:3030/ds/query", params=payload1)
+    return json2html.convert(json = r1.json(), table_attributes="class=\"table table-bordered table-hover\"")
+
+def q2():
+
+    query2 = """
+    SELECT ?recipe_name ?object ?ingredient_name
+    WHERE {
+      	?subject ?predicate recipe:Christmas.
+        ?subject recipe:Recipe_Name ?recipe_name.
+        ?subject recipe:Has_Nationality recipe:Venezuelan.
+        ?subject recipe:Has ?object.
+        ?object recipe:Ingredient_Name ?ingredient_name
+    }
+    """
+    payload2 = {'query': prefix + query2}
+    r2 = requests.get("http://localhost:3030/ds/query", params=payload2)
+    return json2html.convert(json = r2.json(), table_attributes="class=\"table table-bordered table-hover\"")
+
+def q3():
+
+    query3 = """
+    SELECT ?recipe_name (COUNT(?ingredient_name) as ?ingredient_count)
+    WHERE {
+         ?subject recipe:Recipe_Name ?recipe_name.
+         ?subject recipe:Has ?object.
+         ?object recipe:Ingredient_Name ?ingredient_name
+    }
+    GROUP BY ?recipe_name
+    HAVING (?ingredient_count < 4)
+    """
+    payload3 = {'query': prefix + query3}
+    r3 = requests.get("http://localhost:3030/ds/query", params=payload3)
+    return json2html.convert(json = r3.json(), table_attributes="class=\"table table-bordered table-hover\"")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,4 +75,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,'static/')
+print STATIC_URL
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/css"),
+)
+print STATICFILES_DIRS
